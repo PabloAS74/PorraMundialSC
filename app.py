e5d0ba5e8813e92d70f4022d2ba1aef2ded28cce
PabloAS74 <andressalgadopablo@gmail.com>
2026-06-13 00:29:14 +0200

    exposición a cualquier ip

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ def comparativa():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
